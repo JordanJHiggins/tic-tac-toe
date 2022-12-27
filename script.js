84df390ae4190d1094c543d5ade6033ed4ba766d
Jordanjhiggins <jordan.higgins14@gmail.com>
2022-12-27 14:33:16 -0400

    add function that maps selection to correct array index

--- script.js
+++ script.js
@@ -2,9 +2,9 @@
 const gameBoardModule = (function () {
   const gameBoard = {
     gameBoardArray: [
-      ["x", "o", "x"],
-      ["x", "o", "o"],
-      ["x", "x", "x"],
+      ["", "", ""],
+      ["", "", ""],
+      ["", "", ""],
     ],
   };
 
@@ -12,18 +12,61 @@ const gameBoardModule = (function () {
 })();
 
 const displayControllerModule = (function () {
+  const gameBoardContainer = document.querySelector("#game-board");
+  const boardCell = document.createElement("div");
+
   const displayController = {
     renderBoard: function () {
-      const gameBoardContainer = document.querySelector("#game-board");
-      const boardCell = document.createElement("div");
-      boardCell.className = "board-cell";
-
-      //   gameBoardContainer.innerHTML = gameBoardModule.gameBoard.gameBoardArray;
-      for (let i = 0; i < 9; i++)
+      for (let i = 0; i < 9; i++) {
+        boardCell.setAttribute("data-board-cell", i);
+        boardCell.className = "board-cell";
         gameBoardContainer.insertAdjacentElement(
           "beforeend",
           boardCell.cloneNode(true)
         );
+      }
+    },
+    renderSelction: function () {
+      const selectionMap = {
+        0: "0",
+        1: "0",
+        2: "0",
+        3: "1",
+        4: "1",
+        5: "1",
+        6: "2",
+        7: "2",
+        8: "2",
+      };
+
+      const innerArrayMap = {
+        0: "0",
+        1: "1",
+        2: "2",
+        3: "0",
+        4: "1",
+        5: "2",
+        6: "0",
+        7: "1",
+        8: "2",
+      };
+      gameBoardContainer.addEventListener("click", (event) => {
+        let mapValue = event.target.getAttribute("data-board-cell");
+
+        let outerArrayValue = selectionMap[mapValue];
+        let innerArrayValue = innerArrayMap[mapValue];
+
+        console.log(`outer : ${outerArrayValue}`);
+
+        console.log(`inner: ${innerArrayValue}`);
+
+        gameBoardModule.gameBoard.gameBoardArray[outerArrayValue].splice(
+          innerArrayValue,
+          1,
+          "x"
+        );
+        // event.target.innerHTML = `${gameBoardModule.gameBoard.gameBoardArray[1][2]}`;
+      });
     },
     renderPlayers: function () {},
   };
@@ -43,3 +86,5 @@ const player = (name) => {
 };
 
 displayControllerModule.displayController.renderBoard();
+displayControllerModule.displayController.renderSelction();
+console.log(gameBoardModule.gameBoard.gameBoardArray[2][1]);
