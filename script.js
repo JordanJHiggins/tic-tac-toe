42b5d541dc1501ee3ab65ef2848e829458ec4039
Jordanjhiggins <jordan.higgins14@gmail.com>
2022-12-27 14:43:15 -0400

    move code for better module organization

--- script.js
+++ script.js
@@ -8,7 +8,31 @@ const gameBoardModule = (function () {
     ],
   };
 
-  return { gameBoard };
+  const outerArrayMap = {
+    0: "0",
+    1: "0",
+    2: "0",
+    3: "1",
+    4: "1",
+    5: "1",
+    6: "2",
+    7: "2",
+    8: "2",
+  };
+
+  const innerArrayMap = {
+    0: "0",
+    1: "1",
+    2: "2",
+    3: "0",
+    4: "1",
+    5: "2",
+    6: "0",
+    7: "1",
+    8: "2",
+  };
+
+  return { gameBoard, outerArrayMap, innerArrayMap };
 })();
 
 const displayControllerModule = (function () {
@@ -27,45 +51,18 @@ const displayControllerModule = (function () {
       }
     },
     renderSelction: function () {
-      const selectionMap = {
-        0: "0",
-        1: "0",
-        2: "0",
-        3: "1",
-        4: "1",
-        5: "1",
-        6: "2",
-        7: "2",
-        8: "2",
-      };
-
-      const innerArrayMap = {
-        0: "0",
-        1: "1",
-        2: "2",
-        3: "0",
-        4: "1",
-        5: "2",
-        6: "0",
-        7: "1",
-        8: "2",
-      };
       gameBoardContainer.addEventListener("click", (event) => {
         let mapValue = event.target.getAttribute("data-board-cell");
 
-        let outerArrayValue = selectionMap[mapValue];
-        let innerArrayValue = innerArrayMap[mapValue];
-
-        console.log(`outer : ${outerArrayValue}`);
-
-        console.log(`inner: ${innerArrayValue}`);
+        let outerArrayValue = gameBoardModule.outerArrayMap[mapValue];
+        let innerArrayValue = gameBoardModule.innerArrayMap[mapValue];
 
         gameBoardModule.gameBoard.gameBoardArray[outerArrayValue].splice(
           innerArrayValue,
           1,
           "x"
         );
-        // event.target.innerHTML = `${gameBoardModule.gameBoard.gameBoardArray[1][2]}`;
+        event.target.innerHTML = "x";
       });
     },
     renderPlayers: function () {},
@@ -87,4 +84,3 @@ const player = (name) => {
 
 displayControllerModule.displayController.renderBoard();
 displayControllerModule.displayController.renderSelction();
-console.log(gameBoardModule.gameBoard.gameBoardArray[2][1]);
