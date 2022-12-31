109471db0c39cb28f52bb1c225640c5769fbee71
Jordanjhiggins <jordan.higgins14@gmail.com>
2022-12-31 13:07:22 -0400

    add function to check for winner

--- script.js
+++ script.js
@@ -34,6 +34,7 @@ const gameBoardModule = (function () {
 
   const gameLogic = {
     roundTracker: 1,
+
     playerMove: function (playerName, containerEvent) {
       let mapValue = containerEvent.target.getAttribute("data-board-cell");
       let outerArrayValue = gameBoardModule.outerArrayMap[mapValue];
@@ -48,22 +49,23 @@ const gameBoardModule = (function () {
         playerName,
         containerEvent
       );
-      // event.target.innerHTML = playerName;
     },
+
     rounds: function (containerEvent) {
       const playerOne = player("X");
       const playerTwo = player("O");
-
+      // gameLogic.gameOver();
       if (gameLogic.roundTracker % 2 === 0) {
-        containerEvent.target.classList.add("spaceTaken");
+        containerEvent.target.classList.add("spaceTakenO");
 
         gameLogic.playerMove(playerTwo.getName(), containerEvent);
         displayControllerModule.displayController.renderTurn(
           playerOne.getName()
         );
+
         gameLogic.roundTracker += 1;
       } else {
-        containerEvent.target.classList.add("spaceTaken");
+        containerEvent.target.classList.add("spaceTakenX");
 
         gameLogic.playerMove(playerOne.getName(), containerEvent);
         displayControllerModule.displayController.renderTurn(
@@ -72,13 +74,40 @@ const gameBoardModule = (function () {
         gameLogic.roundTracker += 1;
       }
     },
+
+    winCombo: (winCombo = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ]),
+
+    checkWin: function () {
+      const allCell = document.querySelectorAll("[data-board-cell]");
+      return gameBoardModule.gameLogic.winCombo.some((combination) => {
+        return combination.every((index) => {
+          return allCell[index].classList.contains(
+            "spaceTakenX" || "spaceTakenO"
+          );
+        });
+      });
+    },
   };
+
   // Check if space is taken
   const gameBoardContainer = document.querySelector("#game-board");
   gameBoardContainer.addEventListener("click", (event) => {
-    if (event.target.classList.contains("spaceTaken")) return;
+    if (event.target.classList.contains("spaceTakenX" || "spaceTaken)4"))
+      return;
 
     gameLogic.rounds(this.event);
+    if (gameBoardModule.gameLogic.checkWin()) {
+      console.log("win");
+    }
   });
 
   return { gameBoard, outerArrayMap, innerArrayMap, gameLogic };
